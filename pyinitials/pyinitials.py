dbbe1b0d9d7cc35df6d7328c80f65609d8646d78
Rob van der Leek <robvanderleek@gmail.com>
2021-09-19 14:35:43 +0200

    Working towards first release

--- pyinitials/pyinitials.py
+++ pyinitials/pyinitials.py
@@ -1,2 +1,91 @@
-def initials(fullname: str) -> str:
-    return 'JD'
+import re
+from functools import cmp_to_key, reduce
+
+
+def initials(s: str) -> str:
+    if _is_uppercase_only(s):
+        return s
+    preferred = _preferred_initials(s)
+    if preferred:
+        return preferred
+    if _is_email_address(s):
+        s = re.sub(r'@\S+[.]\S+', '', s)
+    s = _remove_email_address(s)
+    first_letters = [w[0] for w in re.findall(r'\w+', s)]
+    result = ''.join(first_letters)
+    if len(result) >= 2:
+        return result
+    else:
+        all_options = _get_all_initials_for_name(s)
+        for o in all_options:
+            if len(o) >= 2:
+                return o
+        return all_options[-1]
+
+
+def _get_all_initials_for_name(n: str):
+    parts = re.compile(r'\s+').split(n)
+    all_initials = [_get_all_initials_for_word(p) for p in parts]
+    all_combined = _combine_all(all_initials)
+    all_sorted = all_combined.copy()
+
+    def compare_func(a, b):
+        diff = len(a) - len(b)
+        if diff == 0:
+            return all_combined.index(a) - all_combined.index(b)
+        else:
+            return diff
+
+    all_sorted.sort(key=cmp_to_key(compare_func))
+
+    def reduce_func(a, b):
+        if len(a) == 0 or len(b) > len(a[-1]):
+            return a + [b]
+        else:
+            return a
+
+    return reduce(reduce_func, all_sorted, [])
+
+
+def _is_uppercase_only(fullname: str):
+    uppercase_letters_only_pattern = '^[A-Z]+$'
+    return re.match(uppercase_letters_only_pattern, fullname) is not None
+
+
+def _preferred_initials(fullname: str):
+    initials_in_name_pattern = '[(]([^[)]+)[)]'
+    match = re.search(initials_in_name_pattern, fullname)
+    if match:
+        return match.group(1)
+
+
+EMAIL_PATTERN = r'\S+@\S+[.]\S+'
+
+
+def _is_email_address(s: str) -> bool:
+    s = s.strip()
+    return re.match(f'^{EMAIL_PATTERN}$', s) is not None
+
+
+def _remove_email_address(s: str) -> str:
+    return re.sub(EMAIL_PATTERN, '', s)
+
+
+def _get_all_initials_for_word(s: str) -> list[str]:
+    result = []
+    for i in range(1, len(s) + 1):
+        result.append(s[0:i])
+    return result
+
+
+def _combine_all(l: list):
+    current = l.pop(0)
+    results = []
+    if len(l) > 0:
+        next = _combine_all(l)
+        for c in current:
+            for n in next:
+                results.append(c + n)
+        return results
+    else:
+        return current
--- tests/test_pyinitials.py
+++ tests/test_pyinitials.py
@@ -1,5 +1,42 @@
 from pyinitials import initials
+from pyinitials.pyinitials import _preferred_initials, _is_uppercase_only, _is_email_address, \
+    _get_all_initials_for_word, _combine_all, _get_all_initials_for_name
 
 
-def test_all():
+def test_initials():
     assert initials('John Doe') == 'JD'
+    assert initials('john doe') == 'jd';
+    assert initials('John Doe <joe@example.com>') == 'JD'
+    assert initials('joe@example.com') == 'jo'
+    assert initials('John Doe (dj)') == 'dj'
+    assert initials('안형준') == '안형'
+
+
+def test_preferred_initials():
+    assert _preferred_initials('John Doe (dj)') == 'dj'
+
+
+def test_is_uppercase_only():
+    assert _is_uppercase_only('JD') is True
+    assert _is_uppercase_only('Jd') is False
+
+
+def test_is_email_pattern():
+    assert _is_email_address('robvanderleek@gmail.com') is True
+    assert _is_email_address('  robvanderleek@gmail.com') is True
+    assert _is_email_address('robvanderleek@gmail.com  ') is True
+
+
+def test_get_all_initials_for_word():
+    assert _get_all_initials_for_word('rob') == ['r', 'ro', 'rob']
+
+
+def test_combine_all():
+    l = [['r', 'ro'], ['l', 'le']]
+    assert _combine_all(l) == ['rl', 'rle', 'rol', 'role']
+
+
+def test_get_all_initials_for_name():
+    result = _get_all_initials_for_name('John Doe')
+    expected = ['JD', 'JDo', 'JDoe', 'JoDoe', 'JohDoe', 'JohnDoe']
+    assert result == expected
